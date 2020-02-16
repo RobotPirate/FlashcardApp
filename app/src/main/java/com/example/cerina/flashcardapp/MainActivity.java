2b6bc888385375a11802b696b6a20f5f72e7fe47
Cerina da Graca <dagraca@usc.edu>
2020-02-15 17:42:21 -0800

    Added customizations and icons

--- app/src/main/java/com/example/cerina/flashcardapp/MainActivity.java
+++ app/src/main/java/com/example/cerina/flashcardapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.cerina.flashcardapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,20 +12,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Change view to answer card when question card gets clicked on
         findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                findViewById(R.id.rootView).setBackgroundColor(getResources().getColor(R.color.colorLightBlueBG));
                 findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
                 findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
             }
         });
 
+        //Change view back to question card when answer card gets clicked on
         findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                findViewById(R.id.rootView).setBackgroundColor(getResources().getColor(R.color.colorWhite));
                 findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
                 findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
             }
         });
+
+//        ImageView icon = findViewById(R.id.toggle_choices_visibility);
+//        icon.setImageResource(R.drawable.ic_presidential_eagle);
     }
 }
