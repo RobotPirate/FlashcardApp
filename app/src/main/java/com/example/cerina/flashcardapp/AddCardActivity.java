90e0e40176c2ee265a466a8ec82d268c069a7f14
Cerina da Graca <dagraca@usc.edu>
2020-03-07 14:33:08 -0800

    moved some functions around

--- app/src/main/java/com/example/cerina/flashcardapp/AddCardActivity.java
+++ app/src/main/java/com/example/cerina/flashcardapp/AddCardActivity.java
@@ -16,6 +16,11 @@ public class AddCardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_card);
 
+        //Grab the 2 strings from input
+        final String questionInput = ((EditText) findViewById(R.id.addQuestionText)).getText().toString();
+        final String answerInput = ((EditText) findViewById(R.id.addAnswerText)).getText().toString();
+
+
         ImageView closeButton = findViewById(R.id.closeButton);
         closeButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -28,17 +33,19 @@ public class AddCardActivity extends AppCompatActivity {
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Grab the 2 strings from input
-                String questionInput = ((EditText) findViewById(R.id.addQuestionText)).getText().toString();
-                String answerInput = ((EditText) findViewById(R.id.addAnswerText)).getText().toString();
+
+//                if(){
+//
+//                }
+//                else{
+//
+//                }
 
                 //Put strings into Intent to pass to MainActivity
                 Intent data = new Intent(); // create a new Intent, this is where we will put our data
                 data.putExtra("question", questionInput); // puts one string into the Intent, with the key as 'question'
                 data.putExtra("answer", answerInput); // puts another string into the Intent, with the key as 'answer'
                 setResult(RESULT_OK, data); // set result code and bundle data for response
-
-
                 finish(); // closes and pass data to the original activity that launched this activity (MainActivity)
             }
         });
